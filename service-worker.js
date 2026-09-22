86b59bf810c4e15ba0f7379c6b33e3918fb6433e
kekestudiogame-droid <kekestudiogame@gmail.com>
2026-09-22 23:20:42 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+// =====================================================
+// KERJIVA SERVICE WORKER
+// =====================================================
+
+const CACHE_NAME = "kerjiva-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./language.js",
+  "./manifest.json",
+  "./kerjiva.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
